021b9e6777d6a6e4f8eaf8607a38c49ac39b1717
Cholewa98 <cholewka.michal@gmail.com>
2020-11-16 18:59:28 +0100

    fix transactions model

--- src/api/transactions/transactions.routes.js
+++ src/api/transactions/transactions.routes.js
@@ -7,22 +7,26 @@ const transactionsModel = require('./transactions.model');
 
 // Create new transactions
 
-// router.post('/', veryfy, async (req, res) => {
-//     try {
-//         const newTransaction = new transactionsModel({
-//             category: req.body.category,
-//             room: req.body.room,
-//             income: req.body.income
-//         });
+router.post('/', veryfy, async (req, res) => {
+    try {
+        const newTransaction = new transactionsModel({
+            transactionName: req.body.transactionName,
+            transactionDescription: req.body.transactionDescription,
+            transactionValue: req.body.transactionValue,
+            transactionDate: req.body.transactionDate,
+            category: req.body.category,
+            room: req.body.room,
+            income: req.body.income
+        });
 
-//         const savedTransaction = await newTransaction.save();
+        const savedTransaction = await newTransaction.save();
         
-//         res.status(201).json(savedTransaction); 
+        res.status(201).json(savedTransaction); 
 
-//     } catch(err) {
-//         res.status(400).json({message: err.message});
-//     }
-// });
+    } catch(err) {
+        res.status(400).json({message: err.message});
+    }
+});
 
 
 module.exports = router;
